753ddec414aacff96523e863dfd88c2933a6ddb7
sanjeev <sanjeevkumarsahani123@gmail.com>
2021-12-13 20:35:05 +0530

    basic,jsx,component,spliting code into components,composition

---
+++ expense-app/src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import Card from './Card';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+const ExpenseItem = (props) => {
+   return (
+      <Card className='expense-item'>
+         <ExpenseDate date={props.item.date} />
+         <div className='expense-item__description'>
+            <h2>{props.item.title}</h2>
+            <div className='expense-item__price'>${props.item.amount}</div>
+         </div>
+      </Card>
+   );
+};
+export default ExpenseItem;
---
+++ expense-app/src/components/Expenses.js
@@ -0,0 +1,15 @@
+import Card from './Card';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+const Expenses = (props) => {
+   return (
+      <Card className='expenses'>
+         <ExpenseItem item={props.items[0]} />
+         <ExpenseItem item={props.items[1]} />
+         <ExpenseItem item={props.items[2]} />
+         <ExpenseItem item={props.items[3]} />
+      </Card>
+   );
+};
+
+export default Expenses;
